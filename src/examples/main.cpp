d07ba55efc4a4c24754e9ca14891a2639b0cb6c0
Filipp Andjelo <filipp.andjelo@gmail.com>
2023-12-22 00:36:03 +0100

    Add example

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -3,12 +3,19 @@
 
 int main(int argc, const char** argv) {
 
+    // How to create and fill a database
     sqlite3pp::Database db{":memory:"};
     db.execute("CREATE TABLE foo (a,b,c)");
     db.execute("INSERT INTO foo VALUES (1,2,3), (2,3,4), (3,4,5)");
-    const auto results = db.prepare("SELECT a,c FROM foo").execute<std::map<int,int>>();
-    for (const auto& result : results) {
-        std::cout << result.first << ',' << result.second << '\n';
+
+    // How to read values from the database into a map
+    for (const auto& [key, value] : db.execute<std::map<int,int>>("SELECT a,c FROM foo")) {
+        std::cout << key << ',' << value << '\n';
     }
+
+    // How to use prepared statement
+    const auto stmt = db.prepare("SELECT a FROM foo WHERE c = ? LIMIT 1");
+    stmt.bind(1, 5);
+    std::cout << stmt.execute<int>() << '\n';
     return 0;
 }

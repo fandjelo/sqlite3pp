af382ab2f26f5237a798391f684350adebab6238
Filipp Andjelo <filipp.andjelo@gmail.com>
2023-12-22 00:20:01 +0100

    Add experimental conanfile

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMake
+
+class Pkg(ConanFile):
+    name = "database"
+    version = "0.1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+    requires = [ "sqlite3/[>=3.8]" ]
+    test_requires = [ "gtest/[>=1.12]" ]
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        if can_run(self):
+            cmake.test()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()

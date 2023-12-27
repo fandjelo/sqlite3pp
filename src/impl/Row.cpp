6e99dcb1457d29f71d743f212a8c9cdf03a76049
Filipp Andjelo <filipp.andjelo@gmail.com>
2023-12-27 13:05:38 +0100

    Fixed clang-tidy warnings

--- src/impl/Row.cpp
+++ src/impl/Row.cpp
@@ -27,40 +27,40 @@
 
 namespace sqlite3pp {
 
-    std::size_t Row::get(std::size_t index, int& value) const {
-        if (SQLITE_INTEGER != sqlite3_column_type(m_stmt, index)) {
-            throw TypeMismatchError{index};
-        }
-        value = sqlite3_column_int(m_stmt, index);
-        return index + 1;
+std::size_t Row::get(std::size_t index, int& value) const {
+    if (SQLITE_INTEGER != sqlite3_column_type(m_stmt, static_cast<int>(index))) {
+        throw TypeMismatchError{index};
     }
+    value = sqlite3_column_int(m_stmt, static_cast<int>(index));
+    return index + 1;
+}
 
-    std::size_t Row::get(std::size_t index, double& value) const {
-        if (SQLITE_FLOAT != sqlite3_column_type(m_stmt, index)) {
-            throw TypeMismatchError{index};
-        }
-        value = sqlite3_column_double(m_stmt, index);
-        return index + 1;
+std::size_t Row::get(std::size_t index, double& value) const {
+    if (SQLITE_FLOAT != sqlite3_column_type(m_stmt, static_cast<int>(index))) {
+        throw TypeMismatchError{index};
     }
+    value = sqlite3_column_double(m_stmt, static_cast<int>(index));
+    return index + 1;
+}
 
-    std::size_t Row::get(std::size_t index, std::string& value) const {
-        if (SQLITE_TEXT != sqlite3_column_type(m_stmt, index)) {
-            throw TypeMismatchError{index};
-        }
-        const auto length = sqlite3_column_bytes(m_stmt, index);
-        const auto* text = sqlite3_column_text(m_stmt, index);
-        value.assign(text, text + length);
-        return index + 1;
+std::size_t Row::get(std::size_t index, std::string& value) const {
+    if (SQLITE_TEXT != sqlite3_column_type(m_stmt, static_cast<int>(index))) {
+        throw TypeMismatchError{index};
     }
+    const auto* text = sqlite3_column_text(m_stmt, static_cast<int>(index));
+    const auto length = sqlite3_column_bytes(m_stmt, static_cast<int>(index));
+    value.assign(text, text + length); // NOLINT: bridge to C-code
+    return index + 1;
+}
 
-    std::size_t Row::get(std::size_t index, std::vector<char>& value) const {
-        if (SQLITE_BLOB != sqlite3_column_type(m_stmt, index)) {
-            throw TypeMismatchError{index};
-        }
-        const auto length = sqlite3_column_bytes(m_stmt, index);
-        const auto* data = static_cast<const char*>(sqlite3_column_blob(m_stmt, index));
-        value.assign(data, data + length);
-        return index + 1;
+std::size_t Row::get(std::size_t index, std::vector<char>& value) const {
+    if (SQLITE_BLOB != sqlite3_column_type(m_stmt, static_cast<int>(index))) {
+        throw TypeMismatchError{index};
     }
+    const auto* data = static_cast<const char*>(sqlite3_column_blob(m_stmt, static_cast<int>(index)));
+    const auto length = sqlite3_column_bytes(m_stmt, static_cast<int>(index));
+    value.assign(data, data + length); // NOLINT: bridge to C-code
+    return index + 1;
+}
 
 } // namespace sqlite3pp
